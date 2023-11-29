c8a180ca4afae8f164dc6a4dde9398fd5e96d260
“suryabk” <surya27bk@gmail.com>
2023-11-30 01:09:09 +0700

    feat: home screen page

---
+++ lib/pages/home_screen.dart
@@ -0,0 +1,75 @@
+import 'package:book_app/model/book_data.dart';
+import 'package:book_app/pages/detail_screen.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Book App'),
+        ),
+        body: const BookList());
+  }
+}
+
+class BookList extends StatelessWidget {
+  const BookList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        final Book book = bookList[index];
+        return InkWell(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => DetailScreen(book: book)),
+            );
+          },
+          child: Card(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Expanded(flex: 2, child: Image.asset(book.imageAsset)),
+                Expanded(
+                    flex: 3,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            book.name,
+                            style: const TextStyle(
+                                fontSize: 18.0,
+                                fontFamily: 'Poppins',
+                                fontWeight: FontWeight.w700),
+                          ),
+                          Container(
+                              margin: const EdgeInsets.only(bottom: 8),
+                              child: Text(
+                                book.author,
+                                style: const TextStyle(fontFamily: 'Poppins'),
+                              )),
+                          Text(
+                            book.shortDescription,
+                            style: const TextStyle(
+                                fontFamily: 'Poppins',
+                                fontWeight: FontWeight.w300),
+                          )
+                        ],
+                      ),
+                    ))
+              ],
+            ),
+          ),
+        );
+      },
+      itemCount: bookList.length,
+    );
+  }
+}
