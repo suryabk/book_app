2783fcace100bad15bd55d7a7847c8f2e19cd847
“suryabk” <surya27bk@gmail.com>
2023-11-30 01:10:20 +0700

    feat: detail screen page

---
+++ lib/pages/detail_screen.dart
@@ -0,0 +1,12 @@
+import 'package:book_app/model/book_data.dart';
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  final Book book;
+  const DetailScreen({required this.book});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
